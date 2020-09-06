2360e19b30c923648055b60f15fb9f6cfe13102f
a.kozeruk <openlexx@gmail.com>
2020-09-06 21:04:05 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { ErrorScreenContainer } from "@containers";
+import { ErrorScreenContainer, RegistrationForm } from "@containers";
 import { MakeWordPage } from "@pages";
-import { RegistrationForm } from "@containers/RegistrationForm/RegistrationForm";
 
 export const App: React.FC<{}> = () => {
     return (
