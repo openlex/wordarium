17b67813731e7d35b494f231e674e050943c721a
a.kozeruk <openlexx@gmail.com>
2020-09-08 01:06:03 +0300

    redirecrt

--- src/components/ui/Title/Title.test.tsx
+++ src/components/ui/Title/Title.test.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { render } from "enzyme";
+import {shallow} from "enzyme";
 import { Title } from "@components";
 
 describe("Title markup", () => {
     it("match snapshoot", () => {
-        expect(render(<Title level={1}>Title</Title>)).toMatchSnapshot();
-        expect(render(<Title level={2}>Title</Title>)).toMatchSnapshot();
-        expect(render(<Title level={3}>Title</Title>)).toMatchSnapshot();
-        expect(render(<Title level={4}>Title</Title>)).toMatchSnapshot();
+        expect(shallow(<Title level={1}>Title</Title>)).toMatchSnapshot();
+        expect(shallow(<Title level={2}>Title</Title>)).toMatchSnapshot();
+        expect(shallow(<Title level={3}>Title</Title>)).toMatchSnapshot();
+        expect(shallow(<Title level={4}>Title</Title>)).toMatchSnapshot();
     });
 });
