74b309590e17b684cbbee45314853757b9568302
a.kozeruk <openlexx@gmail.com>
2020-09-09 23:19:56 +0300

    fixes

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Header } from "@components";
 import { text, withKnobs } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
-import { emptyBlue } from "@components/ui/ProgressCircle/ProgressCircle.stories";
 
 export default {
     title: "Хэдер страницы",
